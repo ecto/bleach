ca4623ede5d9fffc027855aad2fd33628ecd6329
Cam Pedersen <diffference@gmail.com>
2011-09-26 18:59:53 -0400

    started adding sanitization tests

--- test/analyze.js
+++ test/analyze.js
@@ -16,11 +16,30 @@ vows.describe('bleach').addBatch({
         analyze();
       }, Error);
     },
+    'only accepts strings': function(analyze) {
+      assert.throws(function(){
+        analyze(Object);
+      }, Error);
+    },
     'finds self-closing tags': function(analyze){
       assert.ok(analyze('<input type="text" />').length > 0);
     },
     'extracts attributes': function(analyze){
       assert.equal(analyze('<input type="text"></input>')[0].attr[0], { name: 'type', value: '"text"' }.toString());
     }
+  },
+  'bleach.sanitize()': {
+    topic: function(){ return bleach.sanitize; },
+    'is a function': function(sanitize) {
+      assert.equal(typeof sanitize, 'function');
+    },
+    'does not require options': function(sanitize){
+      assert.doesNotThrow(function(){
+        sanitize('');
+      });
+    },
+    'returns a string': function(sanitize) {
+      assert.isArray(sanitize(' '));
+    },
   }
 }).export(module);
